f88fc6b8648cdc89a3665627a07fa56ecfe7e592
JMoney-216 <jdm216@gmail.com>
2020-01-15 12:00:00 -0600

    react mastermind class project

---
+++ src/components/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SettingsPage = props => (
+  <>
+    <Link to="/">Home</Link>
+    <h1>Settings Page</h1>
+  </>
+);
+
+export default SettingsPage;
